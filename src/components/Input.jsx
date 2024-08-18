da5ebc92ac22570ba47cd18498fed7420ad44105
Stanescu Victor <stanescuvictor12@gmail.com>
2024-08-18 21:55:23 +0200

    production ip added, must be improved only temporary solution

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,7 +7,7 @@ export function Input() {
   const [messageChatBot, setMessageChatBot] = useState("")
   
   const sendInputServer = (inputValue) =>{
-    fetch(`http://localhost:3002/query`, {
+    fetch(`http://49.13.128.218:3002/query`, {
       method: "POST",
       headers: {
         "Content-Type":"application/json",
